a67081437c28cd3a0d1674f8367a83eb53d292a3
jamesrivett <jamesrivett@users.noreply.github.com>
2020-09-15 14:07:46 -0400

    Fuckit
    revamping the whole site

--- js/script.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    $(window).scroll(function () {
-        var oVal;
-        oVal = $(window).scrollTop() / 500;
-        return $('.blur').css('opacity', oVal);
-    });
-}.call(this));
